8c12cffb8b240aede733ff65ac211746b14982b3
Craig Ryan <craig@craigryan.org>
2020-09-23 14:30:54 +0000

    Implement email.js integration on contact page

--- Resume-Project/assets/js/sendEmail.js
+++ Resume-Project/assets/js/sendEmail.js
@@ -1,5 +1,5 @@
 function sendMail(contactForm) {
-    emailjs.send("Rosie", "rosie", {
+    emailjs.send("gmail", "rosie", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
@@ -13,3 +13,4 @@ function sendMail(contactForm) {
         });
         return false;
 }
+
